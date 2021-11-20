2b29e012c05d18826e261494284899e1cbd8a0fe
reatured <61943376+reatured@users.noreply.github.com>
2021-11-19 18:02:09 -0800

    Finished

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+paper.install(window);
+paper.setup('canvas');
+canvas.setAttribute("width", "600");
+canvas.setAttribute("height", "600");
+
+var myCircle = new Path.Circle(new Point(300, 300), 300);
+myCircle.fillColor = 'orange';
